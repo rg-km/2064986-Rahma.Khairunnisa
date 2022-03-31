bb301fc55cc38c758ef6ffc05b7336ee1ca1d177
Rahmaakn <rahmaaeyy@gmail.com>
2022-03-31 09:25:42 +0700

    finish work

--- backend/basic-golang/interface/1-defining-interface-cp/main.go
+++ backend/basic-golang/interface/1-defining-interface-cp/main.go
@@ -12,14 +12,42 @@ import "fmt"
 // Bonus untuk JuniorEngineer adalah 3 * BaseSalary
 
 // TODO: answer here
+type Employee interface {
+	FindByName(name string) (Employee, error)
+	FindByOrigin(origin string) ([] Employee, error)
+}
+ 
+type Employee struct {
+	Name string
+	Origin string
+}
 
+type EmployeeWithMap struct {
+	data map[string]Employee
+}
 
-func TotalEmployeeBonus(employees []Employee) int {
+func Employee (employees []Employee) int {
 	// Hitunglah total bonus yang dikeluarkan dari list of Employee
 	// TODO: answer here
+	fmt.Println("FindByName from Employee")
+	// implementations..
+	return Employee{}, nil
 }
 
 func main() {
 	// Buatlah objek konkret untuk masing-masing objek dan panggil function TotalEmployeeBonus. Print total bonus untuk semua employee.
 	// TODO: answer here
+	var Employee Employee
+
+	// Employee menggunakan map
+	Employee =EmployeeWithMap{
+		data: map[string]Employee{},
+	}
+	Employee.FindByName("Manager")
+
+	// Employee menggunakan MySQL
+	EmployeeWithMySQL := 
+	Employee = EmployeeWithMySQL{}
+
+
 }
--- backend/basic-golang/interface/2-empty-interface-cp/main.go
+++ backend/basic-golang/interface/2-empty-interface-cp/main.go
@@ -10,6 +10,15 @@ func main() {
 
 	// Buatlah beberapa menu yang memiliki atribut nama, jenis (nasi, cepat saji, minuman, dll), dan harga. Tambahkan setiap menu pada list `menu`
 	// TODO: answer here
+	data = 100
+	fmt.Println(data)
+
+	data := 
+	data = "Restaurant"
+	fmt.Println(data)
+
+	data = true
+	fmt.Println(data)
 
 	for _, m := range menu {
 		for k, v := range m {
