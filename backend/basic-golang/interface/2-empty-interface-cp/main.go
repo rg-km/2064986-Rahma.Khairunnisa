db6c33a45ec3ef9ad202b3f503d62fed6fbbb665
Rahmaakn <rahmaaeyy@gmail.com>
2022-04-04 21:01:45 +0700

    create package cp

--- backend/basic-golang/interface/2-empty-interface-cp/main.go
+++ backend/basic-golang/interface/2-empty-interface-cp/main.go
@@ -12,8 +12,8 @@ func GetMenu() []map[string]interface{} {
 	var menu []map[string]interface{}
 
 	// TODO: answer here
-	menu = append(menu, map[string]interface{}{"Nama": "Ikan Bakar", "Jenis": "Olahan", "Harga": 40000})
-	menu = append(menu, map[string]interface{}{"Nama": "Rujak Buah", "Jenis": "Camilan", "Harga": 5000})
+	menu = append(menu, map[string]interface{}{"Nama": "Ayam Goreng", "Jenis": "Cepat Saji", "Harga": 20000})
+	menu = append(menu, map[string]interface{}{"Nama": "Cola", "Jenis": "Minuman", "Harga": 7000})
 
 	return menu
 }
