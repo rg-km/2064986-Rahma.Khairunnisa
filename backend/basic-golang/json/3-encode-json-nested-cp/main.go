5ba06efbb2dced1d1f24aa9407d217283f903fdd
Rahmaakn <rahmaaeyy@gmail.com>
2022-04-05 10:12:03 +0700

    encode json cp

--- backend/basic-golang/json/3-encode-json-nested-cp/main.go
+++ backend/basic-golang/json/3-encode-json-nested-cp/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"log"
+
 )
 
 // Dari contoh sebelumnya
@@ -32,10 +33,16 @@ import (
 
 type Ukuran struct {
 	// TODO: answer here
+	Panjang string `json:"panjang"`
+	Tinggi  string `json:"tinggi"`
 }
 
 type Meja struct {
 	// TODO: answer here
+	Jenis  string `json:"jenis"`
+	Warna  string `json:"warna"`
+	Jumlah int    `json:"jumlah"`
+	Ukuran Ukuran `json:"ukuran"`
 }
 
 type Items struct {
@@ -44,6 +51,31 @@ type Items struct {
 
 func (m Items) EncodeJSON() string {
 	// TODO: answer here
+	items := [ ]Meja{
+		{
+			Jenis:  "Meja Makan",
+			Warna:  "Coklat",
+			Jumlah: 20,
+			Ukuran: Ukuran{
+				Panjang: "50 cm",
+				Tinggi:  "25 cm",
+			},
+		},
+		{
+			Jenis:  "Meja Lipat",
+			Warna:  "Hitam",
+			Jumlah: 1,
+			Ukuran: Ukuran{
+				Panjang: "70 cm",
+				Tinggi:  "30 cm",
+			},
+		},
+	}
+	mejaJSON, err := json.Marshal(items)
+	if err != nil {
+	log.Fatal("JSON Marshal error: ", err)
+	}
+	return string(mejaJSON)
 }
 
 func NewMeja(m Items) Items {
