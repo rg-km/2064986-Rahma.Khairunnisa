3b887d7dcdbc469e5c3963126001d9d88a33a1a0
Rahmaakn <rahmaaeyy@gmail.com>
2022-04-07 22:46:17 +0700

    data evaluation

--- backend/basic-golang/template/4-function-cp/main.go
+++ backend/basic-golang/template/4-function-cp/main.go
@@ -25,9 +25,75 @@ type Leaderboard struct {
 
 func CalculateScore(leaderboard Leaderboard) int {
 	// TODO: answer here
+	total := 0
+
+	for _, user := range leaderboard.Users {
+		total += user.Score
+	}
+	return total
 }
 
 func ExecuteToByteBuffer(leaderboard Leaderboard) ([]byte, error) {
-	var textTemplate string
 	// TODO: answer here
+	funcMap := template.FuncMap{
+		"CalculateScore": CalculateScore,
+	}
+
+	textTemplate := `{{ range .Users }}{{ .Name }}: {{ .Score }}{{ end }}Total Score: {{ CalculateScore . }}`
+
+	var b bytes.Buffer
+
+	tmpl, err := template.New("test").Funcs(funcMap).Parse(textTemplate)
+	if err != nil {
+		return nil, err
+	}
+
+	err = tmpl.Execute(&b, leaderboard)
+	if err != nil {
+		return nil, err
+	}
+
+	return b.Bytes(), nil
+
+}
+
+func main() {
+	users := []*UserRank{
+		{
+			Name:  "Roger",
+			Rank:  1,
+			Score: 1000,
+		},
+		{
+			Name:  "Tony",
+			Rank:  2,
+			Score: 900,
+		},
+		{
+			Name:  "Bruce",
+			Rank:  3,
+			Score: 800,
+		},
+		{
+			Name:  "Natasha",
+			Rank:  4,
+			Score: 700,
+		},
+		{
+			Name:  "Clint",
+			Rank:  5,
+			Score: 600,
+		},
+	}
+
+	leaderboardObject := Leaderboard{
+		Users: users,
+	}
+
+	b, err := ExecuteToByteBuffer(leaderboardObject)
+	if err != nil {
+		panic(err)
+	}
+
+	println(string(b))
 }
