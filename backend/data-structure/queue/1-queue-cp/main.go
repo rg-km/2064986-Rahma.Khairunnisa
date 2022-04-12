102dd81a5cca0171c1b1f0973e1fba4dfc08011c
Rahmaakn <rahmaaeyy@gmail.com>
2022-04-12 14:46:05 +0700

    queue 1

--- backend/data-structure/queue/1-queue-cp/main.go
+++ backend/data-structure/queue/1-queue-cp/main.go
@@ -49,5 +49,17 @@ func main() {
 
 func CountStudents(students []int, sandwiches []int) int {
 	 // TODO: replace this
+	count := [2]int{}
+	for i := 0; i < len(students); i++ {
+		count[students[i]]++
+	}
+	for i := 0; i < len(sandwiches); i++ {
+		if count[sandwiches[i]] > 0 {
+			count[sandwiches[i]]--
+		} else {
+			break
+		}
+	}
+	return count[0] + count[1]
 
 }
