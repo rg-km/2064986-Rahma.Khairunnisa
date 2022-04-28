177f40d6de8f5ea9f2ec88672809d6d7460a524e
Rahmaakn <rahmaaeyy@gmail.com>
2022-04-28 20:21:15 +0700

    ASSIGNMENT SEBELUM LEBARAN SELESAI WOII

--- backend/basic-golang/cashier-app/api/auth.go
+++ backend/basic-golang/cashier-app/api/auth.go
@@ -47,57 +47,50 @@ func (api *API) login(w http.ResponseWriter, req *http.Request) {
 
 	// TODO: answer here
 	expirationTime := time.Now().Add(5 * time.Minute)
-		claims := &Claims{
-			Username: creds.Username,
-			StandardClaims: jwt.StandardClaims{
-				ExpiresAt: expirationTime.Unix(),
-			},
-		}
-
 	// Task: Buat token menggunakan encoded claim dengan salah satu algoritma yang dipakai
 
 	// TODO: answer here
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-
+	claims := &Claims{
+		Username: *res,
+		StandardClaims: jwt.StandardClaims{
+			// expiry time menggunakan time millisecond
+			ExpiresAt: expirationTime.Unix(),
+		},
+	}
 	// Task: 1. Buat jwt string dari token yang sudah dibuat menggunakan JWT key yang telah dideklarasikan
 	//       2. return internal error ketika ada kesalahan ketika pembuatan JWT string
 
 	// TODO: answer here
-	tokenString, err := token.SignedString(jwtKey)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	// Task: Set token string kedalam cookie response
 
 	// TODO: answer here
+	tokenString, err := token.SignedString(jwtKey)
+		if err != nil {
+			// return internal error ketika ada kesalahan ketika pembuatan JWT string
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
 	http.SetCookie(w, &http.Cookie{
 		Name:    "token",
 		Value:   tokenString,
 		Expires: expirationTime,
-	})
-
+		})
 	// Task: Return response berupa username dan token JWT yang sudah login
 
-
-	//json.NewEncoder(w).Encode(LoginSuccessResponse{Username : *res})  // TODO: replace this
-	
-	//json.NewEncoder(w).Encode(LoginSuccessResponse{Username: "", Token: ""}) // TODO: replace this
+	json.NewEncoder(w).Encode(LoginSuccessResponse{Username: claims.Username, Token: tokenString}) // TODO: replace this
 }
 
 func (api *API) logout(w http.ResponseWriter, req *http.Request) {
 	api.AllowOrigin(w, req)
 	username := req.URL.Query().Get("username")
 	err := api.usersRepo.Logout(username)
-	encoder := json.NewEncoder(w)
 	if err != nil {
-
 		w.WriteHeader(http.StatusUnauthorized)
-		
+		encoder := json.NewEncoder(w)
 		encoder.Encode(AuthErrorResponse{Error: err.Error()})
 		return
 	}
 
-	// encoder.Encode(AuthErrorResponse{Error: ""}) // TODO: replace this
+	w.WriteHeader(http.StatusOK) // TODO: replace this
 }
