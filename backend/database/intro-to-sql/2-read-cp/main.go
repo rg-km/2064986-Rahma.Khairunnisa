25b99159c156918669a5105c9cfcc5b2863a5e4e
Rahmaakn <rahmaaeyy@gmail.com>
2022-05-24 19:48:03 +0700

    sql 2

--- backend/database/intro-to-sql/2-read-cp/main.go
+++ backend/database/intro-to-sql/2-read-cp/main.go
@@ -15,7 +15,6 @@ func NewEmployeeRepository(db *sql.DB) *EmployeeRepository {
 }
 
 func (r *EmployeeRepository) FetchEmployeeByNIK(nik string) (*model.Employee, error) {
-	var sqlStmt string
 
 	// Task:
 	// buat query untuk mengambil data employee berdasarkan nik
@@ -24,11 +23,11 @@ func (r *EmployeeRepository) FetchEmployeeByNIK(nik string) (*model.Employee, er
 	// data diambil dengan nik tertentu dari parameter nik
 
 	// TODO: answer here
-	sqlStmt = `
+	sqlStmt := `
 	SELECT 
-	    id, nik, firstname, lastname, email
+	    id, nik, first_name, last_name, email
     FROM 
-        employee 
+        employees 
     WHERE nik = ?`
 
 	row := r.db.QueryRow(sqlStmt, nik)
@@ -48,7 +47,7 @@ func (r *EmployeeRepository) FetchEmployeeByNIK(nik string) (*model.Employee, er
 }
 
 func (r *EmployeeRepository) FetchEmployees() ([]model.Employee, error) {
-	var sqlStmt string
+
 
 	// Task:
 	// buat query untuk mengambil data employees
@@ -56,11 +55,11 @@ func (r *EmployeeRepository) FetchEmployees() ([]model.Employee, error) {
 	// lihat model.Employee untuk field yang diambil dari database
 
 	// TODO: answer here
-	sqlStmt = `
+	sqlStmt := `
 	SELECT 
-	    id, nik, firstname, lastname, email
+	    id, nik, first_name, last_name, email
 	FROM 
-		employee`
+		employees`
 
 	rows, err := r.db.Query(sqlStmt)
 	if err != nil {
